f02d9207ec12a5b21ee2463a7a7f134093a5e6d4
scajasc <sac.cabrera@yavirac.edu.ec>
2019-09-03 11:10:10 -0500

    Pantalla de movimiento de Inventario

---
+++ src/ec/edu/epn/laboratorios/converter/TipoOrdenInvSelectItemConverter.java
@@ -0,0 +1,26 @@
+package ec.edu.epn.laboratorios.converter;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.FacesConverter;
+
+import org.omnifaces.converter.SelectItemsConverter;
+
+import ec.edu.epn.laboratorioBJ.entities.LaboratorioLab;
+import ec.edu.epn.laboratorioBJ.entities.Tipordeninv;
+import ec.edu.epn.laboratorioBJ.entities.Tiposervicio;
+
+
+@FacesConverter(forClass=Tipordeninv.class)
+public class TipoOrdenInvSelectItemConverter extends SelectItemsConverter{
+	
+	@Override
+	public String getAsString(FacesContext context, UIComponent component,
+			Object value) {
+		Integer id = (value instanceof Tipordeninv) ? ((Tipordeninv) value).getIdTipordeninv()
+				: null;
+		return (id != null) ? String.valueOf(id) : null;
+
+	}
+
+}
